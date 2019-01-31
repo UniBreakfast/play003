4850fe4d95e208771e500df58f370753f1c53c19
UniBreakfast <unibreakfast1@gmail.com>
2019-01-31 17:53:47 +0200

    added time screen

--- play003/script.js
+++ play003/script.js
@@ -1,46 +1,57 @@
 function mur(what) { console.log(what) }
-function check() {
-  if (!login.value && !mail.value) return response.innerText =
-    "Чтобы войти введите ваш Логин или Электронную почту"
-  if (!pass.value) return response.innerText =
-    "Без пароля войти нельзя"
-  if (!mail.value) {
-    for (let user of state.users) {
-      if (user.login == login.value) {
-        if (user.pass == pass.value) return response.innerText = "Заходим"
-        else {
-          pass.value = ''
-          return response.innerText = "Неправильный пароль"
-        }
-      }
-    }
-    login.value = ''
-    return response.innerText = "Пользователь с таким Логином не найден"
-  }
-  if (!login.value) {
-    for (let user of state.users) {
-      if (user.mail == mail.value) {
-        if (user.pass == pass.value) return response.innerText = "Заходим"
-        else {
-          pass.value = ''
-          return response.innerText = "Неправильный пароль"
-        }
-      }
-    }
-    mail.value = ''
-    return response.innerText =
-      "Пользователь с такой Электронной почтой не найден"
-  }
-  for (let user of state.users) {
-    if (user.mail==mail.value || user.login==login.value) {
-      if (user.pass == pass.value) return response.innerText = "Заходим"
-      else {
-        pass.value = ''
-        return response.innerText = "Неправильный пароль"
-      }
+
+function rit(str) { response.innerText = str }
+
+function SignUP() {
+  if (!login.value && !mail.value)
+    return rit("Для регистрации нужен Логин и/или Электронная почта")
+  if (!pass.value) return rit("Придумайте Пароль, который не сможете забыть")
+  if (login.value) {
+    for (let u of state.users) {
+      if (u.login==login.value) return rit("Этот Логин уже занят")
     }
   }
-  login.value = mail.value = ''
-  return response.innerText =
-    "Пользователь с таким Логином или Электронной почтой не найден"
+  if (mail.value) {
+    for (let u of state.users) {
+      if (u.mail==mail.value)
+        return rit("Пользователь с этой Электронной почтой уже есть")
+    }
+  }
+  let user = {pass: pass.value}
+  if (login.value) user.login = login.value
+  if ( mail.value) user.mail  =  mail.value
+  state.users.push(user)
+  mur(JSON.stringify(state, null, 2))
+  return rit("Пользователь зарегистрирован. Заходим")
+}
+
+function SignIN() {
+  if (!login.value && !mail.value)
+    return rit("Чтобы войти введите ваш Логин или Электронную почту")
+  if (!pass.value) return rit("Без Пароля войти нельзя")
+  if (!mail.value) {   // введён только логин
+    for (let u of state.users) {
+      if (u.login==login.value) {
+        if (u.pass==pass.value) return rit("Заходим")
+        else return rit("Неправильный Пароль")
+      }
+    }
+    return rit("Пользователь с таким Логином не найден")
+  }
+  if (!login.value) {   // введён только мэйл
+    for (let u of state.users) {
+      if (u.mail==mail.value) {
+        if (u.pass==pass.value) return rit("Заходим")
+        else return rit("Неправильный Пароль")
+      }
+    }
+    return rit("Пользователь с такой Электронной почтой не найден")
+  }
+  for (let u of state.users) {   // введены и логин, и мэйл
+    if (u.mail==mail.value || u.login==login.value) {
+      if (u.pass==pass.value) return rit("Заходим")
+      else return rit("Неправильный Пароль")
+    }
+  }
+  return rit("Пользователь с таким Логином или Электронной почтой не найден")
 }
